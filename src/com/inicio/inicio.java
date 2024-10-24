0332d585f186752de617d4915b1a6b4c6fcbd7a8
maico <maico@MAICOL>
2024-10-24 13:30:12 -0500

    materias asigandas con sus variables

--- src/com/inicio/inicio.java
+++ src/com/inicio/inicio.java
@@ -239,7 +239,7 @@ try {
                 ventanaAlumno.setVisible(true);
                 this.setVisible(false);
             } else if (priv.equals("profesor")) {
-                Materias ventanaDocente = new Materias(nombre,idUsuario);
+                materiasp ventanaDocente = new materiasp(nombre,idUsuario);
                 ventanaDocente.setVisible(true);
                 this.setVisible(false);
             }
